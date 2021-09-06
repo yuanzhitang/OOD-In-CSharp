cb0a12fc3b60693331b55cc80a38763cff8f3faa
TangYM <Yuanzhi.Tang@cn.unisys.com>
2021-09-06 22:59:42 +0800

    Add Empty Design-Principles Project

---
+++ Design-Principles/Single-Responsibility-Principle/SPRDemo.cs
@@ -0,0 +1,11 @@
+﻿namespace Design_Principles.Single_Responsibility_Principle
+{
+	public class SPRDemo
+	{
+		public void Run()
+		{
+			//UserBadSample.Run();
+			//UserGoodSample.Run();
+		}
+	}
+}
